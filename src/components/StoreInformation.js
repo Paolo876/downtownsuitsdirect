260f633db9a59385b92581d1078101d6e69c8f0e
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-18 20:48:52 -0800

    updated StoreInformation component

--- src/components/StoreInformation.js
+++ src/components/StoreInformation.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import { useDocument } from '../hooks/useDocuments';
-import { Container, Card, CardContent, Grid, Typography } from '@mui/material';
+import { Container, Card, CardContent, Grid, Typography, Button } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import PhoneIcon from '@mui/icons-material/Phone';
@@ -9,17 +10,22 @@ import "./StoreInformation.scss";
 
 const StoreInformation = () => {
   const { document, error, isLoading } = useDocument("store-data", "information")
-  
+  const navigate = useNavigate();
 
   if(document) return (
     <Container className='store-information'>
         <Grid container spacing={2} sx={{my:4}}>
-            <Grid item xs={12} sx={{textAlign: "center", letterSpacing: ".1em", mb: 2}}>
+            <Grid item xs={12} sx={{textAlign: "center", letterSpacing: ".1em", mb: 5}}>
                 <h1>Downtown suits direct</h1>
                 <hr/>
-                </Grid>
+            </Grid>
             <Grid item xs={12} md={4}>
-                <p>1</p>
+                <div className='header'><h3>Store address:</h3></div>
+                <div className='body'>
+                    <p>{document.address.address1} {document.address.address2}</p>
+                    <p>{document.address.city}, {document.address.state} {document.address.zip}</p>
+                </div>
+                <Button variant="outlined"><a href={document.address.googleMapUrl}><PlaceIcon/> Get Directions</a></Button>
             </Grid>
             <Grid item xs={12} md={4}>
                 <p>2</p>
