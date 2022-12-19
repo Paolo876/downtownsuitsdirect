bf80f11aea382cdc8c624cea88f2eeb55489eee7
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-18 21:04:01 -0800

    updated store information component

--- src/components/StoreInformation.js
+++ src/components/StoreInformation.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useDocument } from '../hooks/useDocuments';
-import { Container, Card, CardContent, Grid, Typography, Button } from '@mui/material';
+import { Container, Card, CardContent, Grid, Typography, Button, Link } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import PlaceIcon from '@mui/icons-material/Place';
 import "./StoreInformation.scss";
 
@@ -14,8 +16,8 @@ const StoreInformation = () => {
 
   if(document) return (
     <Container className='store-information'>
-        <Grid container spacing={2} sx={{my:4}}>
-            <Grid item xs={12} sx={{textAlign: "center", letterSpacing: ".1em", mb: 5}}>
+        <Grid container spacing={2} sx={{ my:4 }}>
+            <Grid item xs={12} sx={{textAlign: "center", letterSpacing: ".1em", mb: 5, }}>
                 <h1>Downtown suits direct</h1>
                 <hr/>
             </Grid>
@@ -28,7 +30,15 @@ const StoreInformation = () => {
                 <Button variant="outlined"><a href={document.address.googleMapUrl}><PlaceIcon/> Get Directions</a></Button>
             </Grid>
             <Grid item xs={12} md={4}>
-                <p>2</p>
+            <div className='header'><h3>Contact Information:</h3></div>
+                <div className='body'>
+                    <p><EmailIcon/> {document.email}</p>
+                    <p><PhoneIcon/> {document.phone}</p>
+                    <p><Link href={document.social.facebook}><FacebookIcon/> Facebook</Link></p>
+                    <p><Link href={document.social.instagram}><InstagramIcon/> Instagram</Link></p>
+                </div>
+                <Button variant="outlined"><a href={document.address.googleMapUrl}><CalendarMonthIcon/> Book an Appointment</a></Button>
+
             </Grid>
             <Grid item xs={12} md={4}>
                 <p>3</p>
