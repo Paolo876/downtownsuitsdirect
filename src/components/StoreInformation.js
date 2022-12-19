6f09a6d78a1ea325af9b9c3b0439ac2efccffc6c
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-19 00:10:44 -0800

    added store hours on storeInformation component

--- src/components/StoreInformation.js
+++ src/components/StoreInformation.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useDocument } from '../hooks/useDocuments';
-import { Container, Card, CardContent, Grid, Typography, Button, Link } from '@mui/material';
+import { Container, Grid, Button, Link, List, ListItem, ListItemText } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import PersonIcon from '@mui/icons-material/Person';
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
@@ -14,7 +13,6 @@ import "./StoreInformation.scss";
 const StoreInformation = () => {
   const { document, error, isLoading } = useDocument("store-data", "information")
   const navigate = useNavigate();
-
   if(document) return (
     <Container className='store-information'>
         <Grid container spacing={2} sx={{ my:4 }}>
@@ -24,21 +22,6 @@ const StoreInformation = () => {
             </Grid>
 
             <Grid item xs={12} md={4} sx={{mt:3}}>
-                <h3 className='header'>Store Address:</h3>
-                <Grid container alignItems={{xs: "flex-start", md: "center"}}>
-                    <Grid item xs={6} md={12}>
-                        <p>{document.address.address1} {document.address.address2}</p>
-                        <p>{document.address.city}, {document.address.state} {document.address.zip}</p>
-                    </Grid>
-                    <Grid item xs={6} md={12} sx={{mt:{xs: 0, md: 3}}}>
-                        <Button href={document.address.googleMapUrl} variant="outlined" className='action-button'>
-                            <PlaceIcon sx={{mr: 1}} fontSize="small"/> Get Directions
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={12} md={4} sx={{mt:3}}>
-                <h3 className='header'>Contact:</h3>
                 <Grid container alignItems={{xs: "flex-start", md: "center"}}>
                     <Grid item xs={6} md={12}>
                         <h4 className='header'>{document.name}</h4>
@@ -56,18 +39,28 @@ const StoreInformation = () => {
                         <Button className='action-button' variant='outlined'><CalendarMonthIcon sx={{mr: 1}} fontSize="small"/>Schedule an Appointment</Button>
                     </Grid>
                 </Grid>
-                {/* <div className='body'>
-                    <p className='link-item'><strong>{document.name}</strong></p>
-                    <p className='link-item'><PhoneIcon/> {document.phone}</p>
-                    <Link href={`mailto: ${document.email}`} className='link-item'><EmailIcon/> {document.email}</Link>
-                    <Link href={document.social.facebook} className='link-item'><FacebookIcon/> Facebook</Link>
-                    <Link href={document.social.instagram} className='link-item'><InstagramIcon/> Instagram</Link>
-                </div> */}
-                {/* <Button variant="outlined"><CalendarMonthIcon/>Book an Appointment</Button> */}
-
             </Grid>
             <Grid item xs={12} md={4} sx={{mt:3}}>
-                <p>3</p>
+                <h3 className='header'>Store Address:</h3>
+                <Grid container alignItems={{xs: "flex-start", md: "center"}}>
+                    <Grid item xs={6} md={12}>
+                        <p>{document.address.address1} {document.address.address2}</p>
+                        <p>{document.address.city}, {document.address.state} {document.address.zip}</p>
+                    </Grid>
+                    <Grid item xs={6} md={12} sx={{mt:{xs: 0, md: 3}}}>
+                        <Button href={document.address.googleMapUrl} variant="outlined" className='action-button'>
+                            <PlaceIcon sx={{mr: 1}} fontSize="small"/> Get Directions
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} md={4} sx={{mt:3}}>
+                <h3 className='header'>Store Hours:</h3>
+                <List>
+                    {document["store-hours"].map(item => (
+                        <ListItem key={item.day} sx={{m:0, p:0}}><ListItemText>{item.day}</ListItemText> <ListItemText>{item.from} - {item.to}</ListItemText></ListItem>
+                    ))}
+                </List>
             </Grid>
         </Grid>
     </Container>
