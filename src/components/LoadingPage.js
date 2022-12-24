f1c3595cd2afdb24a293d29696327935c6ad1872
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-23 22:36:35 -0800

    added loadingPage component

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./LoadingPage.scss"
+
+const LoadingPage = () => {
+  return (
+    <div className='loading-page'>
+        <span className="loader"></span>  
+    </div>
+  )
+}
+
+export default LoadingPage
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import GalleryShowcase from '../components/GalleryShowcase';
 import Navbar from "../components/Navbar"
 import StoreInformation from '../components/StoreInformation'
 import YelpReviews from '../components/YelpReviews';
+import LoadingPage from '../components/LoadingPage';
 import { useDocument } from '../hooks/useDocuments';
 import { useCollection } from '../hooks/useCollection';
 const Home = () => {
   // const { document, error, isLoading } = useDocument("store-data", "information");
   const { documents } = useCollection("store-data")
 
+  return <LoadingPage/>
   if(documents) return (
     <>
       <DocumentHead title="Downtown Suits Direct | Home"/>
