25e703c4ce5de83d7fff70029b40ddacf7d89c82
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-24 23:33:34 -0800

    updated about component

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                 <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize="1.8em">The Store</Typography>
                 <hr/>
             </Grid>
-            <Grid item xs={6} sx={{textAlign: "center", letterSpacing: ".1em", mb: 2, }}>
+            <Grid item xs={12} s={6} sx={{textAlign: "center", letterSpacing: ".1em", mb: 2, }}>
                 <Typography variant="body2" gutterBottom align='center' fontWeight="light" fontSize="1em">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Laborum rem id nulla velit quaerat praesentium a nisi. \
@@ -17,13 +17,7 @@ const About = () => {
                     dolorem atque quisquam doloremque sequi, unde, iste quia temporibus minus consectetur. 
                     Quidem impedit excepturi iste cumque distinctio eligendi facere delectus aspernatur illum praesentium.
                 </Typography>
-                <Typography variant="body2" gutterBottom align='center' fontWeight="light" fontSize="1em">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Laborum rem id nulla velit quaerat praesentium a nisi. \
-                    Ab accusantium neque facilis aut amet autem assumenda ex dignissimos, 
-                    dolorem atque quisquam doloremque sequi, unde, iste quia temporibus minus consectetur. 
-                    Quidem impedit excepturi iste cumque distinctio eligendi facere delectus aspernatur illum praesentium.
-                </Typography>
+
             </Grid>
         </Grid>
     </Container>
