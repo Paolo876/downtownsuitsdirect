912b88e961634ac109c9df41dc2d9e11115507d7
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-27 01:28:50 -0800

    updated appcontext

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -9,23 +9,25 @@ const initialState = {
     gallery: null,
     information: null,
     imagekitKeys: null,
-    isLoading: false,
+    isLoading: true,
     error: null,
 };
 
 const appReducer = (state, action) => {
     switch (action.type) {
+        case 'INIT':
+            return {...state, ...action.payload }
         case 'SET_DATA':
           return { ...state, error: null, gallery: action.payload.gallery, information: action.payload.information }
+        case 'SET_IMAGEKITKEYS':
+            return { ...state, error: null, imagekitKeys: action.payload }
         case 'SET_IS_LOADING':
-          return { ...state, error: null, isLoading: action.payload }
+            return { ...state, error: null, isLoading: action.payload }
         case 'SET_ERROR':
             return { ...state, error: action.payload, isLoading: false }
-        // case 'ALLOW_LOGIN':
-        //   return { ...state, isLoginAllowed: action.payload }
         default:
           return state
-      }
+    }
 }
 
 export const AppContextProvider = ({ children }) => {
@@ -33,17 +35,19 @@ export const AppContextProvider = ({ children }) => {
     const { documents, error } = useCollection("store-data");
     useEffect(() => {
         if(documents) {
-            dispatch({ type: 'SET_DATA', payload: documents })
-            dispatch({ type: 'SET_IS_LOADING', payload: false })
+            // dispatch({ type: 'SET_DATA', payload: documents })
+            // dispatch({ type: 'SET_IS_LOADING', payload: false })
+            init()
         }
         if(!documents && !error) dispatch({ type: 'SET_IS_LOADING', payload: true })
         if(error) dispatch({ type: 'SET_ERROR', payload: error })
     }, [documents]);
 
     const init = async () => {
-        //set store-data collection to state
-        //get imagekit keys
+        const { data } = await httpsCallable(functions, "getImagekitKeys")()
+        dispatch({ type: 'INIT', payload: { imagekitKeys: data, gallery: documents.gallery, information: documents.information, isLoading: false }})
     }
+
     return (
         <AppContext.Provider value={{ ...state, dispatch }}>
           { children }
