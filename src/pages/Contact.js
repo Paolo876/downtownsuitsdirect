4439c979192f43f6a4de943a3abeb83ef35120e0
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-27 21:34:30 -0800

    updated contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import { Container } from '@mui/material'
 
 const Contact = () => {
   return (
     <>
       <Navbar/>
+        <Container>
+          
+        </Container>
+      <Footer/>
     </>
   )
 }
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,17 +4,15 @@ import Footer from "../components/Footer";
 import { Container, Typography } from '@mui/material';
 
 const Gallery = () => {
-  return (
-    <>
-      <Navbar/>
-      <Container sx={{ pt: 5 }}>
-        <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize={{xs: "1.45em", md: "1.8em" }}>Gallery</Typography>
-        <hr/>
-      </Container>
-      <GalleryImages/>
-      <Footer/>
-    </>
-  )
+  return <>
+    <Navbar/>
+    <Container sx={{ pt: 5 }}>
+      <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize={{xs: "1.45em", md: "1.8em" }}>Gallery</Typography>
+      <hr/>
+    </Container>
+    <GalleryImages/>
+    <Footer/>
+  </>
 }
 
 export default Gallery
