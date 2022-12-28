02e38001e616e8cd055ef8da83ceeef18884fce9
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-28 00:43:44 -0800

    updated contactform

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,32 @@
 import React, { useState } from 'react'
-import { FormControl, InputLabel, Input, Paper, Typography } from '@mui/material';
+import { FormControl, InputLabel, Input, Paper, Typography, Grid, TextField } from '@mui/material';
 
 const ContactForm = () => {
   return (
     <form>
         <Paper sx={{display: "flex", flexDirection: "column", width: "fit-content", p: 5, mx: "auto"}}>
-            <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize={{xs: "1.25em", md: "1.5em" }}>Inquiry form</Typography>
+            <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize={{xs: "1.25em", md: "1.5em" }} mb={2}>Inquiry form</Typography>
             <hr/>
-            <FormControl>
-                <InputLabel htmlFor="my-input">Name</InputLabel>
-                <Input id="my-input"/>
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="my-input">E-mail</InputLabel>
-                <Input id="my-input"/>
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="my-input">Phone <small>(optional)</small></InputLabel>
-                <Input id="my-input"/>
-            </FormControl>
+            <Grid container>
+                <Grid item xs={6}>
+                    <FormControl sx={{my: 1, mt:3}}>
+                        <InputLabel htmlFor="my-input">Name</InputLabel>
+                        <Input id="my-input"/>
+                    </FormControl>
+                    <FormControl sx={{my: 1}}>
+                        <InputLabel htmlFor="my-input">E-mail</InputLabel>
+                        <Input id="my-input"/>
+                    </FormControl>
+                    <FormControl sx={{my: 1}}>
+                        <InputLabel htmlFor="my-input">Phone <small>(optional)</small></InputLabel>
+                        <Input id="my-input"/>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={6}>
+
+                </Grid>
+            </Grid>
+            
         </Paper>
     </form>
   )
