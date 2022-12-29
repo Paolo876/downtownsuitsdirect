735a752dc253eea5d63e994a12108c21eea23362
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-28 22:59:51 -0800

    updated contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,7 +10,6 @@ import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
   const { information } = useAppContext();
-  console.log(information)
   return (
     <>
       <Navbar/>
@@ -21,28 +20,28 @@ const Contact = () => {
           You can reach me using the contact informations provided or by submitting the inquiry form below.<br/>I'm looking forward to hear from you and I'd be happy to answer any questions and inquiries.
         </Typography>
         <Grid container my={6}>
-          <Grid item xs={4} align="center">
-            <Paper  sx={{display: "flex", flexDirection: "column",alignItems: "center", justifyContent: "space-between", width: 200, height: 160, p:2}}>
-              <PlaceIcon sx={{fontSize: "2.5em"}}/>
+          <Grid item xs={12} sm={4} align="center">
+            <Paper  sx={{display: "flex", flexDirection: {xs: "row", sm:"column"}, alignItems: "center", textAlign: {xs: "left", sm: "center"},justifyContent: {xs: "left",sm:"space-between"}, width: { xs: "100%", sm:200}, height: {xs: 100, sm: 160}, p:2, my: 1}}>
+              <PlaceIcon sx={{fontSize: "2.5em", mx: 3}}/>
               <div>
-                <Typography variant="h5" gutterBottom fontWeight="regular" color="textPrimary" mt={1}>Store address</Typography>
+                <Typography variant="h5" fontWeight="regular" color="textPrimary" mt={1}>Store address</Typography>
                 <Typography paragraph fontWeight="regular" color="textSecondary" my={0} mt={1}>{information.address.address1} {information.address.address2}</Typography>
                 <Typography paragraph fontWeight="regular" color="textSecondary" my={1}>{information.address.city}, {information.address.state} {information.address.zip}</Typography>
               </div>
             </Paper>
           </Grid>
-          <Grid item xs={4} align="center">
-            <Paper  sx={{display: "flex", flexDirection: "column",alignItems: "center", justifyContent: "space-between", width: 200, height: 160,p:2}}>
-              <PhoneIcon sx={{fontSize: "2.5em"}}/>
+          <Grid item xs={12} sm={4} align="center">
+            <Paper  sx={{display: "flex", flexDirection: {xs: "row", sm:"column"}, alignItems: "center", textAlign: {xs: "left", sm: "center"},justifyContent: {xs: "left",sm:"space-between"}, width: { xs: "100%", sm:200}, height: {xs: 100, sm: 160}, p:2, my: 1}}>
+              <PhoneIcon sx={{fontSize: "2.5em", mx: 3}}/>
               <div>
                 <Typography variant="h5" gutterBottom fontWeight="regular" color="textPrimary" mt={1}>{information.name}</Typography>
                 <Typography paragraph fontWeight="regular" color="textSecondary" my={1}>{information.phone}</Typography>
               </div>
             </Paper>
           </Grid>
-          <Grid item xs={4} align="center">
-            <Paper  sx={{display: "flex", flexDirection: "column",alignItems: "center", justifyContent: "space-between", width: 200, height: 160, p:2}}>
-              <EmailIcon sx={{fontSize: "2.5em"}}/>
+          <Grid item xs={12} sm={4} align="center">
+            <Paper  sx={{display: "flex", flexDirection: {xs: "row", sm:"column"}, alignItems: "center", textAlign: {xs: "left", sm: "center"},justifyContent: {xs: "left",sm:"space-between"}, width: { xs: "100%", sm:200}, height: {xs: 100, sm: 160}, p:2, my: 1}}>
+              <EmailIcon sx={{fontSize: "2.5em", mx: 3}}/>
               <div>
                 <Typography variant="h5" gutterBottom fontWeight="regular" color="textPrimary" mt={1}>E-mail</Typography>
                 <Typography paragraph fontWeight="regular" color="textSecondary" my={1}>{information.email}</Typography>
