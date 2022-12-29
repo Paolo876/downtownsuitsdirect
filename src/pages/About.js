4d70ca64f6cdf10d30d8cd791461c9413ba96202
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-28 23:18:41 -0800

    udpated about page and services page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { Container, Typography } from '@mui/material';
 
 const About = () => {
   return (
     <>
       <Navbar/>
+      <Container sx={{ py: 5 }}>
+        <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize={{xs: "1.45em", md: "1.8em" }}>About</Typography>
+        <hr/>
+      </Container>
     </>
   )
 }
