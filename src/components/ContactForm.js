5eaf380bc3b124e327ad6dc29cd4015ff720a877
paoloBugarin <paolopaolobugarin@gmail.com>
2022-12-28 22:22:51 -0800

    updated contactform

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,32 +1,41 @@
 import React, { useState } from 'react'
-import { FormControl, InputLabel, Input, Paper, Typography, Grid, TextField } from '@mui/material';
+import { FormControl, InputLabel, Input, Paper, Typography, Grid, TextField, Button } from '@mui/material';
 
 const ContactForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("ASD")
+  }
   return (
-    <form>
-        <Paper sx={{display: "flex", flexDirection: "column", width: "fit-content", p: 5, mx: "auto"}}>
+    <form onSubmit={handleSubmit}>
+        <Paper sx={{display: "flex", flexDirection: "column", width: "100%", p: { xs: 2, md:5 }, mx: "auto", maxWidth: "1000px"}}>
             <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize={{xs: "1.25em", md: "1.5em" }} mb={2}>Inquiry form</Typography>
             <hr/>
-            <Grid container>
-                <Grid item xs={6}>
-                    <FormControl sx={{my: 1, mt:3}}>
-                        <InputLabel htmlFor="my-input">Name</InputLabel>
+            <Grid container mt={2}>
+                <Grid item xs={12} md={6} sx={{display: "flex", flexDirection: "column", width: "100%", p: 2, justifyContent: "flex-end" }}>
+                    <FormControl sx={{ my: 1, mt:3,  }}>
+                        <InputLabel htmlFor="my-input" >Name</InputLabel>
                         <Input id="my-input"/>
                     </FormControl>
-                    <FormControl sx={{my: 1}}>
+                    <FormControl sx={{ my: 1, }}>
                         <InputLabel htmlFor="my-input">E-mail</InputLabel>
                         <Input id="my-input"/>
                     </FormControl>
-                    <FormControl sx={{my: 1}}>
-                        <InputLabel htmlFor="my-input">Phone <small>(optional)</small></InputLabel>
+                    <FormControl sx={{ my: 1,  }}>
+                        <InputLabel htmlFor="my-input">Phone</InputLabel>
                         <Input id="my-input"/>
                     </FormControl>
                 </Grid>
-                <Grid item xs={6}>
-
+                <Grid item xs={12} md={6} sx={{display: "flex", flexDirection: "column", width: "100%", p: 2, justifyContent: "flex-end" }}>
+                    <TextField
+                        sx={{my: 1, mt:3}}
+                        label="Message"
+                        multiline
+                        rows={6}
+                    />
                 </Grid>
             </Grid>
-            
+            <Grid item xs={12} align="center" sx={{ mt: 3 }}><Button type="submit" variant="contained" size='large' sx={{ letterSpacing: ".15em"}}>Submit</Button></Grid>
         </Paper>
     </form>
   )
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,7 +14,7 @@ const Contact = () => {
   return (
     <>
       <Navbar/>
-      <Container sx={{ pt: 5 }}>
+      <Container sx={{ py: 5 }}>
         <Typography variant="h2" gutterBottom align='center' fontWeight="regular" fontSize={{xs: "1.45em", md: "1.8em" }}>Contact</Typography>
         <hr/>
         <Typography variant="body1" gutterBottom align='center' fontWeight="regular" my={2} lineHeight={1.5} px={{xs: 1, md:20}}>
